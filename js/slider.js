a893f8f30d5e1d35b8fcfda28c0205f85f5ed7c0
AlonaZhd <alona.zdakaeva@gmail.com>
2024-02-11 00:29:09 +0100

    add expertise section

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".expertise-slider").slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        variableWidth: true,
+        autoplaySpeed: 5000,
+        cssEase: "ease",
+        pauseOnHover: true,
+    });
+});
