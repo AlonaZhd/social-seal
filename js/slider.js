70936992d5b10a424c68a5d9513f3f52f9b10e7f
AlonaZhd <alona.zdakaeva@gmail.com>
2024-02-14 23:35:38 +0100

    add team section

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function () {
     $(".brand-creation__slider").slick({
+        infinite: true,
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -17,6 +18,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $(".experience__slider").slick({
+        infinite: true,
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -26,5 +28,22 @@ $(document).ready(function () {
         cssEase: "ease",
         pauseOnHover: true,
         centerMode: true,
+    });
+});
+
+/*=============== TEAM ===============*/
+
+$(document).ready(function () {
+    $(".team__slider").slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        // autoplay: true,
+        variableWidth: true,
+        autoplaySpeed: 5000,
+        cssEase: "ease",
+        pauseOnHover: true,
     });
 });
