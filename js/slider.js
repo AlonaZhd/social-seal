82949ca05e68b5974773c89847637cff831f1bf1
AlonaZhd <alona.zdakaeva@gmail.com>
2024-02-14 01:22:54 +0100

    add experience section

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,5 @@
+/*=============== BRAND-CREATION ===============*/
+
 $(document).ready(function () {
     $(".brand-creation__slider").slick({
         arrows: false,
@@ -8,5 +10,21 @@ $(document).ready(function () {
         autoplaySpeed: 5000,
         cssEase: "ease",
         pauseOnHover: true,
+    });
+});
+
+/*=============== EXPERIENCE ===============*/
+
+$(document).ready(function () {
+    $(".experience__slider").slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        variableWidth: true,
+        autoplaySpeed: 8000,
+        cssEase: "ease",
+        pauseOnHover: true,
+        centerMode: true,
     });
 });
